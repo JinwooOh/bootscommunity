edc4969518da2e350db893afb498faf4836dbe84
Jinwoo <ozi0414@gmail.com>
2019-08-09 14:11:51 -0500

    Let's go🔥

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './test';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Test />
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
---
+++ src/test.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React from 'react';
+
+const Test: React.FC = () => (
+  <div className="App">
+    <h2>1234</h2>
+  </div>
+);
+
+export default Test;
